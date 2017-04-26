02967a9afcf14fc46ec0322319781211ab9d18a6
Alsu Islamova <Alsu_Islamova@epam.com>
2017-04-27 01:45:42 +0600

    Add task 7

---
+++ Rewarding/Rewarding/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using Rewarding.App_Start;
+using Rewarding.Migrations;
+using Rewarding.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Rewarding
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundle();
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<PersonContext, Configuration>());
+        }
+    }
+}
